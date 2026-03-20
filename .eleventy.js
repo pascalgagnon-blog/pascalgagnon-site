8fedc29708f763dbf7753edd2f234f1cdcad6904
pascalgagnon-blog <gagnon.wolfric@gmail.com>
2026-03-19 23:49:58 -0400

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,21 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("src/assets");
-    eleventyConfig.addCollection("posts", function(collectionApi) {
-          return collectionApi.getFilteredByGlob("src/posts/*.md").reverse();
-    });
-    return {
-          dir: {
-                  input: "src",
-                  output: "_site",
-                  includes: "_includes"
-          }
-    };
+      eleventyConfig.addPassthroughCopy("src/assets");
+      eleventyConfig.addCollection("posts", function(collectionApi) {
+              return collectionApi.getFilteredByGlob("src/posts/*.md").reverse();
+      });
+      eleventyConfig.addFilter("dateReadable", function(date) {
+              return new Date(date).toLocaleDateString('fr-CA', {
+                        year: 'numeric', month: 'long', day: 'numeric'
+              });
+      });
+      eleventyConfig.addFilter("dateIso", function(date) {
+              return new Date(date).toISOString().split('T')[0];
+      });
+      return {
+              dir: {
+                        input: "src",
+                        output: "_site",
+                        includes: "_includes"
+              }
+      };
 };
